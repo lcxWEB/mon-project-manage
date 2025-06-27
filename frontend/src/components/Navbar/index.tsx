9320e7b5f96af3c320c875e0431a6c0c6d965f29
cx.li <chunxialims@163.com>
2025-06-26 22:09:07 -0700

    hide top seach bar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -29,7 +29,7 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Search Bar */}
-      {/* <div className="flex items-center gap-8">
+      <div className="flex items-center gap-8">
         {!isSidebarCollapsed ? null : (
           <button
             onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
@@ -38,14 +38,14 @@ const Navbar = () => {
           </button>
         )}
         <div className="relative flex h-min w-[200px]">
-          <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
+          {/* <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
             className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
             type="search"
             placeholder="Search..."
-          />
+          /> */}
         </div>
-      </div> */}
+      </div>
 
       {/* Icons */}
       <div className="flex items-center">
